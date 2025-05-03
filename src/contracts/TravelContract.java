9cf482debf9c0e9f3daba8971bba5a0ce240830b
Ліля Журавель <liliazuravel6@gmail..com>
2025-05-03 23:06:03 +0300

    implemented class

--- src/contracts/TravelContract.java
+++ src/contracts/TravelContract.java
@@ -1,2 +1,24 @@
-package contracts;public class TravelContract {
+package contracts;
+
+import company.InsuranceCompany;
+import objects.Person;
+import payment.ContractPaymentData;
+
+import java.util.Set;
+
+public class TravelContract extends AbstractContract{
+    private final Set<Person> insuredPersons;
+
+    public TravelContract(String contractNumber, InsuranceCompany insurer,
+                          Person policyHolder, ContractPaymentData contractPaymentData,
+                          int coverageAmount, Set<Person> personsToInsure) {
+        super(contractNumber, insurer, policyHolder, contractPaymentData, coverageAmount);
+        if (contractPaymentData == null) throw new IllegalArgumentException("Contract payment data can not be null!");
+        if (personsToInsure == null || personsToInsure.isEmpty()) throw new IllegalArgumentException("Person to insure can not be null or empty!");
+        this.insuredPersons = personsToInsure;
+    }
+
+    public Set<Person> getIsuredPersons() {
+        return insuredPersons;
+    }
 }
